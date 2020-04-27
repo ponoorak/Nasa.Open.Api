d4689ca8821e27d12212f2853568d967fe83be8b
Przemysław Śmietana <przemyslaw.smietana@ihsmarkit.com>
2020-04-27 10:31:56 +0200

    refactor build fix

---
+++ src/Nasa.Open.Api.Tests/ApodTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Nasa.Open.Api.Tests
+{
+    public class ApodTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- src/Nasa.Open.Api/Mars/Photos/MarsPhotos.cs
+++ src/Nasa.Open.Api/Mars/Photos/MarsPhotos.cs
@@ -6,43 +6,6 @@
     using Extensions;
     using Models;
 
-    public enum CameraName
-    {
-        FHAZ,       //Front Hazard Avoidance Camera
-        RHAZ,       //Rear Hazard Avoidance Camera
-        MAST,       //Mast Camera
-        CHEMCAM,    //Chemistry and Camera Complex
-        MAHLI,      //Mars Hand Lens Imager
-        MARDI,      //Mars Descent Imager
-        NAVCAM,     //Navigation Camera
-        PANCAM,     //Panoramic Camera
-        MINITES     //Miniature Thermal Emission Spectrometer (Mini-TES)
-    }
-
-    /// <summary>
-    /// This API is designed to collect image data gathered by NASA's Curiosity, Opportunity, and Spirit rovers on Mars
-    /// </summary>
-    public interface IMarsPhotos
-    {
-        /// <summary>
-        /// Querying by Earth date
-        /// </summary>
-        /// <param name="earthDate">corresponding date on earth for the given sol</param>
-        /// <param name="camera"><see cref="CameraName"/></param>
-        /// <param name="page">25 items per page returned</param>
-        /// <returns><see cref="MarsData"/></returns>
-        Task<MarsData> GetAsync(DateTime earthDate, CameraName? camera = null, int page = 1);
-
-        /// <summary>
-        /// Querying by Martian sol
-        /// </summary>
-        /// <param name="sol">sol (ranges from 0 to max found in endpoint)</param>
-        /// <param name="camera"><see cref="CameraName"/></param>
-        /// <param name="page">25 items per page returned</param>
-        /// <returns><c>MarsData</c></returns>
-        Task<MarsData> GetAsync(int sol, CameraName? camera = null, int page = 1);
-    }
-
     [EndPoint(Consts.BASE_URL1, "mars-photos/api/v1/rovers/curiosity/photos")]
     internal class MarsPhotos : Connection, IMarsPhotos
     {
