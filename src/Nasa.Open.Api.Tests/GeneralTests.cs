61e86513e6f9b0ef96e16477feb0a3eba2b55dde
Przemysław Śmietana <przemyslaw.smietana@ihsmarkit.com>
2020-04-27 21:29:57 +0200

    Disable

--- src/Nasa.Open.Api.Tests/GeneralTests.cs
+++ src/Nasa.Open.Api.Tests/GeneralTests.cs
@@ -11,7 +11,7 @@
             Assert.Throws<ArgumentException>(()=> new NasaOpenApi("").Apod.GetAsync().Result);
         }
 
-        [Fact]
+        [Fact(Skip = "Need Internet Connection... fix to moc")]
         public void ApiKeyInvalidTest()
         {
             Assert.Throws<AggregateException>(() => new NasaOpenApi("DEMO_YEK").Apod.GetAsync().Result);
