6ba9398b74c0e150baed41add2c2632e2634e374
Przemysław Śmietana <przemyslaw.smietana@ihsmarkit.com>
2020-04-27 21:04:33 +0200

    refactor

--- src/Nasa.Open.Api.Framework/Program.cs
+++ src/Nasa.Open.Api.Framework/Program.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace Nasa.Open.Api.Framework
 {
+    using Mars.Photos;
+
     class Program
     {
-        private const string Api_key = "DEMO_KEY";
+        private const string ApiKey = "DEMO_KEY";
         static void Main(string[] args)
         {
-            var nasa = new NasaOpenApi(Api_key);
-            var r = nasa.Apod.GetAsync().Result;
-            Console.WriteLine("Hello World!");
+            var nasaOpenApi = new NasaOpenApi(ApiKey);
+
+            var apodService = nasaOpenApi.Apod.GetAsync().Result;
+            var statsService = nasaOpenApi.NeoStats.GetAsync().Result;
+            var marsPhotos = nasaOpenApi.MarsPhotos.GetAsync(1000, CameraName.FHAZ).Result;
+            var earthImage = nasaOpenApi.EarthImage.GetAsync(54.372158, 8.638306).Result;
+            var earthAssets = nasaOpenApi.EarthAssets.GetAsync(54.372158, 8.638306).Result;
+
+            Console.WriteLine("Results from Mars.Open.Api");
+            Console.WriteLine($"Result APOD Url = {apodService.Url}");
+            Console.WriteLine($"Result NEO COUNT = {statsService.NeoCount}");
+            Console.WriteLine($"Result Mars Length = {marsPhotos.Photos.Length}");
+            Console.WriteLine($"Result Earth DataLength = {earthImage.Length}");
+            Console.WriteLine($"Result Earth Assets = {earthAssets.Resource.Dataset}");
+            Console.WriteLine($"Api Remaining Calls = {nasaOpenApi.Remaining}");
+            Console.WriteLine($"Api Limit Calls = {nasaOpenApi.Limit}");
         }
     }
 }
