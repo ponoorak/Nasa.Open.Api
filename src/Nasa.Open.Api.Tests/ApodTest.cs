d0e637b02c231f8b8c5a4eb8436cb09a87962e7e
Przemysław Śmietana <przemyslaw.smietana@ihsmarkit.com>
2020-04-27 21:22:08 +0200

    Test Fix

--- src/Nasa.Open.Api.Tests/ApodTest.cs
+++ src/Nasa.Open.Api.Tests/ApodTest.cs
@@ -10,11 +10,11 @@ namespace Nasa.Open.Api.Tests
         {
             string apiKey = "DEMO_KEY";
 
-            DateTime searchDate = DateTime.Parse("31-12-1999");
+            DateTime searchDate = DateTime.Parse("1999-12-31"); 
 
             string titleExpect = "The Millennium that Defined Earth";
             Uri urlExcept = new Uri("https://apod.nasa.gov/apod/image/9912/earthrise_apollo8.jpg");
-            Uri hdUrlExcept = new Uri("https://apod.nasa.gov/apod/image/9912/earthrise_apollo8.jpg");
+            Uri hdUrlExcept = new Uri("https://apod.nasa.gov/apod/image/9912/earthrise_apollo8_big.gif");
             var actual = new NasaOpenApi(apiKey).Apod.GetAsync(searchDate).Result;
 
             Assert.Equal(titleExpect, actual.Title);
